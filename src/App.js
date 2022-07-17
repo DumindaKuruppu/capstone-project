654d9dcb573f86b48da4d433e8256c7fa73d9990
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2022-07-18 00:26:01 +0530

    Files configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import './style.css';
+import Header from './components/Header';
+import {Routes, Route} from 'react-router-dom';
+import Photos from './pages/Photos';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+    <div>
+        <Header />
+
+        <Routes>
+          <Route exact path="/" element={<Photos />} />
+          <Route path="/cart" element={<Cart />} />
+
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/Photos.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Photos() {
+  return (
+    <div>
+      <main className="photos">
+            <h1>Images Go Here</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Photos;
